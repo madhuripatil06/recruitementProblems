168963910539952a02f834057bf9a5c89c63e880
madhuripatil06 <madhuri@thoughtworks.com>
2016-07-12 18:13:51 +0530

    conference timer is now able to give time for one task

--- timer/tests/TimerTests.py
+++ timer/tests/TimerTests.py
@@ -1,10 +1,9 @@
-from timer.lib.conferrenceTimer import Timer
+from timer.lib.conferrenceTimer import ConferenceTimer
 
 
 def test_when_there_is_only_one_task_in_the_input_then_also_timer_should_give_me_time():
-    time = Timer({"Writing Fast Tests Against Enterprise Rails": 60}).get_time()
+    time = ConferenceTimer().get_time({"Writing Fast Tests Against Enterprise Rails": 60})
     assert time == {
-            "track1":[
+            "Track1":
                 "9.00AM Writing Fast Tests Against Enterprise Rails"
-            ]
     }
